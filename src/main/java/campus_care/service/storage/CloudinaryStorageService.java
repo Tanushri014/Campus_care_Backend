0417e6c3712e9cdd86549c0ca0e728e8833126e1
Tanushri014 <matretanushri0@gmail.com>
2026-07-16 16:39:12 +0530

    fixed dot

--- src/main/java/campus_care/service/storage/CloudinaryStorageService.java
+++ src/main/java/campus_care/service/storage/CloudinaryStorageService.java
@@ -130,7 +130,7 @@ public class CloudinaryStorageService implements FileStorageService {
                 .resourceType(resourceType)
                 .transformation(
                         new com.cloudinary.Transformation<>()
-                                .flags("attachment:"+"notice.pdf")
+                                .flags("attachment:")
                 )
                 .generate(fileName);
         System.out.println("Generated URL = " + url);
